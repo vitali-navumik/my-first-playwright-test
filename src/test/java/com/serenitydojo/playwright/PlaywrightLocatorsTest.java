1c30e205a43238e7f75b9d93a64ed60747698fbb
Vitali <vitali@lorexgrupp.ee>
2026-01-16 22:57:43 +0200

    next try to fix

--- src/test/java/com/serenitydojo/playwright/PlaywrightLocatorsTest.java
+++ src/test/java/com/serenitydojo/playwright/PlaywrightLocatorsTest.java
@@ -34,12 +34,14 @@ public class PlaywrightLocatorsTest {
                                 "--no-sandbox",
                                 "--disable-extensions",
                                 "--disable-gpu",
+                                "--start-maximized",
                                 "--disable-blink-features=AutomationControlled"
                         ))
         );
         // browserContext = browser.newContext();
         browserContext = browser.newContext(new Browser.NewContextOptions()
-                .setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+                .setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+                .setViewportSize(1920, 1080)
         );
     }
 
@@ -370,5 +372,6 @@ public class PlaywrightLocatorsTest {
 
     private void openPage() {
         page.navigate("https://practicesoftwaretesting.com/");
+        page.locator(".navbar").waitFor();
     }
 }
