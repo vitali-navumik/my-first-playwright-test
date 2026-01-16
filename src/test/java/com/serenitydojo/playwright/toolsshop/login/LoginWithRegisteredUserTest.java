163704652875d4d62ebf627c0f5e81193d5eab71
Vitali <vitali@lorexgrupp.ee>
2026-01-17 01:10:23 +0200

    disable should_login_with_registered_user

--- src/test/java/com/serenitydojo/playwright/toolsshop/login/LoginWithRegisteredUserTest.java
+++ src/test/java/com/serenitydojo/playwright/toolsshop/login/LoginWithRegisteredUserTest.java
@@ -4,11 +4,13 @@ import com.serenitydojo.playwright.toolsshop.domain.User;
 import com.serenitydojo.playwright.toolsshop.fixtures.PlaywrightTestCase;
 import com.serenitydojo.playwright.toolsshop.login.pageobjects.LoginPage;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LoginWithRegisteredUserTest extends PlaywrightTestCase {
 
+    @Disabled("Because site block requests from github with captcha")
     @Test
     @DisplayName("Should be able to login with a registered user")
     void should_login_with_registered_user() {
